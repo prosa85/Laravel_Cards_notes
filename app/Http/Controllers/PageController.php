9534bb689c88a3757d8192cb6dcf281be090d366
Pablo Rosa <prosa85@yahoo.com>
2016-04-09 22:36:13 -0400

    Learning Laravel

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+class PageController extends Controller
+{
+    //
+	public function home() {
+    $people = ['Pablo', 'David', 'Steph', 'Sophia'];
+    return view('welcome', compact('people'));
+}
+}
